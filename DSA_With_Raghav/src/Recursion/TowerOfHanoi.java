0827de2c0888ec8d01fc4fd587b8b140c3777fc9
DESAI AMAN <DESAI AMAN@LAPTOP-CGK8OU7U>
2026-08-11 21:53:15 +0530

    commit

--- DSA_With_Raghav/src/Recursion/TowerOfHanoi.java
+++ DSA_With_Raghav/src/Recursion/TowerOfHanoi.java
@@ -17,7 +17,7 @@ public class TowerOfHanoi {
 	{
 		if(n == 0) return;
 		hanoi(n-1,a,c,b);//n-1 a to b use c
-//		System.out.println(a +"->"+c);
+		System.out.println(a +"->"+c);
 		count++;//;largest of A to C
 		hanoi(n-1,b,a,c);//n-1 b to c use a
 		
