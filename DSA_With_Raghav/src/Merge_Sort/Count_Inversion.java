14e9a2d81908d65a43f0082ce74b472f0e1261aa
DESAI AMAN <DESAI AMAN@LAPTOP-CGK8OU7U>
2026-08-12 23:45:47 +0530

    Commit

---
+++ DSA_With_Raghav/src/Merge_Sort/Count_Inversion.java
@@ -0,0 +1,80 @@
+package Merge_Sort;
+
+public class Count_Inversion {
+
+	public static void main(String[] args) {
+		
+		int  arr[] = {5 , 2 , 8 , 4, 1, 6 , 7 , 3};
+		
+		System.out.println(inversionCount(arr));
+	}
+	
+	static int count;
+    public static int inversionCount(int arr[]) {
+        count = 0;
+        mergeSort(arr);
+        return count;
+    }
+    
+    	public static void mergeSort(int arr[]) {
+		
+		int n = arr.length;
+		if(n == 1) return;
+		
+		
+		//new Empty array create 
+		int a[] = new int [n/2];
+		int b[] = new int [ n - n/2];
+		
+		//copy - past array
+		
+		int idx = 0;
+		for(int i = 0; i<a.length; i++) a[i] = arr[idx++];
+		for(int i = 0; i<b.length; i++) b[i] = arr[idx++];
+		
+		//magic sort
+		mergeSort(a);
+		mergeSort(b);
+		
+		
+		merge(a,b,arr);
+					
+	}
+	
+	public static void merge(int a[] , int b[] , int c[]) {
+		
+		int i = 0 , j = 0 , k = 0;
+		
+		 while(i <a.length && j <b.length) {
+			 if(a[i]<=b[j]) {
+				 c[k++] = a[i++];
+			 }
+			 else {
+			     count += (a.length - i);
+				 c[k++] = b[j++];
+			 }
+		 }
+		
+		 while(i<a.length) c[k++] = a[i++];
+		 while(j<b.length) c[k++] = b[j++];
+		
+	}
+
+}
+
+
+/*
+  public static int inversionCount(int arr[]) {
+	        
+	        int count = 0;
+	        for(int  i =0; i<arr.length;i++)
+	        {
+	            for(int j = i+1; j<arr.length; j++ )
+	            {
+	                if(arr[i] > arr[j]) count++;   
+	            }
+	        }
+	        
+	        return count;
+	    }
+ */ 
